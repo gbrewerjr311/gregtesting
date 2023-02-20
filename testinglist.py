9282bbf214269e301fab901226a824b06cd2f9a0
gregbrewer <gbrewer3@my.nl.edu>
2023-02-20 16:44:53 -0500

    Added testinglist.py

---
+++ testinglist.py
@@ -0,0 +1,20 @@
+# numbers = 100
+# for item in range(numbers):
+#     print(item)
+
+l = ['A','B','C','D','E']
+ls = ["chocolate","vanilla","strawberry"]
+
+# for i in ls:
+#     print(i)
+# print(ls)
+
+for index in range(len(l)):
+    print("List item #",index)
+    print("Is actually",l[index])
+
+ls = ["Greg", "Terrence", "Eric", "Dre", "Keith"]
+print(ls[len(ls)-1])
+print(len(ls))
+#for guest in ls:
+#    print(guest)
